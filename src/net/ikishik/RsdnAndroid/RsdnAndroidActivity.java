9de57b098339434441f2ed4cb79b43d9df47dd13
Igor Kishik <ikishik@gmail.com>
2011-06-28 13:36:55 +0400

    first commit

---
+++ src/net/ikishik/RsdnAndroid/RsdnAndroidActivity.java
@@ -0,0 +1,13 @@
+package net.ikishik.RsdnAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RsdnAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
